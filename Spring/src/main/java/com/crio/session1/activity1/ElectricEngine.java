10a4709243bc58b34aff5f980812f9f0fa5e7199
Ayush Muniya <59387871+ayushmuniya@users.noreply.github.com>
2022-12-09 23:59:29 +0530

    Create ElectricEngine.java

---
+++ Spring/src/main/java/com/crio/session1/activity1/ElectricEngine.java
@@ -0,0 +1,9 @@
+package com.crio.session1.activity1;
+
+public class ElectricEngine implements Engine {
+
+    @Override
+    public void turnOn() {
+        System.out.println("Started electric engine");
+    }
+}
