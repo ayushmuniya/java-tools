aec98c1349581fea0a18459e3ffa4c151e30fcf0
Ayush Muniya <59387871+ayushmuniya@users.noreply.github.com>
2023-01-16 23:14:34 +0530

    Create Customer.java

---
+++ Lombok/src/main/java/com/example/lombok/dto/Customer.java
@@ -0,0 +1,13 @@
+package com.example.lombok.dto;
+
+import java.time.LocalTime;
+
+public class Customer 
+{
+    private int customerId;
+    private int cityId;
+    private String address;
+    private String phone;
+    private String email;
+    private LocalTime timeJoined;
+}
