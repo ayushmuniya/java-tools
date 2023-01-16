9c8126190a8f0f1aa6c4ca57e7cf09c16f053208
Ayush Muniya <59387871+ayushmuniya@users.noreply.github.com>
2023-01-16 23:14:51 +0530

    Create Menu.java

---
+++ Lombok/src/main/java/com/example/lombok/dto/Menu.java
@@ -0,0 +1,12 @@
+package com.example.lombok.dto;
+
+import java.util.ArrayList;
+
+public class Menu 
+{
+    private String itemName;
+    private String description;
+    private ArrayList<String> ingredients;
+    private int price;
+    private boolean isOnMenu;
+}
